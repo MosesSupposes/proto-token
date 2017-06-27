a51a877b1788d3b40fbc9764aab483759e4311eb
Moses Samuel <mosamuel98@gmail.com>
2017-06-26 18:49:33 -0700

    refactor code and add metadata

--- protoToken.sol
+++ protoToken.sol
@@ -1,18 +1,29 @@
 contract ProtoToken {
-    // This creates an array with all balances
+    /* Meta data about the contract. In the near future this can be handled by a token registry, but for now we'll add it directly to the contract */
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    
+    /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
 
-    // Initializes contract with initial supply tokens to the creator of the contract. This value can be set on the Ethereum Wallet Client, so we'll leave it blank for now.
-    function ProtoToken(uint256 initialSupply) {
-        balanceOf[msg.sender] = initialSupply;      // Give the creator all initial tokens
+
+    /* Initializes contract with initial supply tokens to the creator of the contract. 
+    This value can be set on the Ethereum Wallet Client, so we'll leave it blank for now. */
+    function ProtoToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
+        balanceOf[msg.sender] = initialSupply;  // Give the creator all initial tokens
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = decimalUnits;
     }
 
-    // Send coins
     function transfer(address _to, uint _value) {
-        if (blanceOf[msg.sender] < _value) throw;               // Check if the sender has enough     
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;    // Check for overflows
-        balanceOf[msg.sender] -= _value;                        // Subract from the sender
-        balanceOf[_to] += _value;                               // Add the same to the recipient
+        /* Check if sender has balance and for overflows */
+        if (balanceOf[msg.sender] < _value) || balanceOf[_to] + _value < balanceOf[_to] 
+            throw;               
+         
+        /* Add and subtract new balances */
+        balanceOf[msg.sender] -= _value;    
+        balanceOf[_to] += _value;           
     }
-
 }
