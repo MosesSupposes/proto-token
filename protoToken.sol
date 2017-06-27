e55b07b457bd8ec02dd15c342b04cbebb5457255
Moses Samuel <mosamuel98@gmail.com>
2017-06-26 17:41:36 -0700

    create minimum viable token

---
+++ protoToken.sol
@@ -0,0 +1,20 @@
+contract ProtoToken {
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+
+    // Initializes contract with initial supply tokens to the creator of the contract
+    function ProtoToken(
+        uint256 initialSupply
+    ) {
+        balanceOf[msg.sender] = initialSupply;      // Give the creator all initial tokens
+    }
+
+    // Send coins
+    function transfer(address _to, uint _value) {
+        if (blanceOf[msg.sender] < _value) throw;               // Check if the sender has enough     
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;    // Check for overflows
+        balanceOf[msg.sender] -= _value;                        // Subract from the sender
+        balanceOf[_to] += _value;                               // Add the same to the recipient
+    }
+
+}
