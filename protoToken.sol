5a1ae99b84b92e5b61e6f58a6d93f4896520f43f
Moses Samuel <mosamuel98@gmail.com>
2017-06-26 19:37:51 -0700

    add an owner subcontract w/ abdication and coin minting capabilities

--- protoToken.sol
+++ protoToken.sol
@@ -1,4 +1,21 @@
-contract ProtoToken {
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
+
+contract ProtoToken is onwed {
     /* Meta data about the contract. In the near future this can be handled by a token registry, but for now we'll add it directly to the contract */
     string public name;
     string public symbol;
@@ -12,8 +29,15 @@ contract ProtoToken {
 
     /* Initializes contract with initial supply tokens to the creator of the contract. 
     This value can be set on the Ethereum Wallet Client, so we'll leave it blank for now. */
-    function ProtoToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
+    function ProtoToken(
+        uint256 initialSupply, 
+        string tokenName, 
+        uint8 decimalUnits, 
+        string tokenSymbol,
+        address centralMinter
+        ) {
         balanceOf[msg.sender] = initialSupply;  // Give the creator all initial tokens
+        if (centralMinter != 0) owner = centralMinter;  // Make the address of the contract's owner the central minter
         name = tokenName;
         symbol = tokenSymbol;
         decimals = decimalUnits;
